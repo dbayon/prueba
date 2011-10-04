b2f2cd516f77ff832bfc77589e9337ade04906c5
David Bayón <david.bayon@softwhisper.com>
2011-10-04 12:14:49 +0200

    segundo commit

---
+++ pruebaGit/MyClass.h
@@ -0,0 +1,13 @@
+//
+//  MyClass.h
+//  pruebaGit
+//
+//  Created by David Bayón on 04/10/11.
+//  Copyright 2011 Softwhisper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyClass : NSObject
+
+@end
---
+++ pruebaGit/MyClass.m
@@ -0,0 +1,23 @@
+//
+//  MyClass.m
+//  pruebaGit
+//
+//  Created by David Bayón on 04/10/11.
+//  Copyright 2011 Softwhisper. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
